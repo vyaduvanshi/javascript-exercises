c274bd12d2aca36385d2c9ad13c21e5e6c98facc
Vikram <vikram.yaduvanshi13@gmail.com>
2024-04-20 14:09:02 +0530

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x,y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((accumulator,current)=>accumulator+current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator,current)=>accumulator*current);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let fact = 1;
+	for (let i=1; i<=x; i++){
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
